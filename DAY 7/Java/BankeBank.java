394c5308fa38aac4d22477fa6aae2d5c3d7bc652
OBOGWUJOSH <165791807+OBOGWUJOSH@users.noreply.github.com>
2024-05-14 18:13:41 +0100

    9

--- DAY 7/Java/BankeBank.java
+++ DAY 7/Java/BankeBank.java
@@ -12,18 +12,34 @@ public static void main(String[] arg){
 
 	Scanner input = new Scanner(System.in);
 	
-	names <String> = new <String>
+	Arraylist <String> names= new Arraylist <String>();
 
 	
 	System.out.print("Welcome To Banke BanK");
+	
+	String Prompt = """
 
+	1. -> Creat an account.
+	2. -> Close account.
+	3. -> Deposite Money.
+	4. -> Withdraw money.
+	5. -> Check account balance .
+	6. -> Transfer from one bank to another.
+	7. -> Change Pin.
+	8. -> Exit App.
+
+	""";
+	
 
 	System.out.println("prompt an option from the menu :" );
 	String prompt = input.nextLine();
 
+	
+
+	
 
 
-
+	
 
 
 
