5627ea24281850b9e4bbcb15309a66f67edee143
OBOGWUJOSH <165791807+OBOGWUJOSH@users.noreply.github.com>
2024-05-16 08:25:02 +0100

    new thursday 05/16/2024

--- DAY 7/Java/BankeBank.java
+++ DAY 7/Java/BankeBank.java
@@ -1,57 +1,156 @@
 
 import java.util.Scanner;
-import java.util.ArrayList;
+public  class BankeBank{
 
-public class BankeBank{
 public static void main(String[] arg){
 
+	menu();
 
 
+}
+
+
+public static void menu(){
 	Scanner input = new Scanner(System.in);
-	
-	ArrayList <String> names = new ArrayList <String>();
-
-	
-	System.out.print("Welcome To Banke BanK");
-
-	String Prompt = """
-	1. -> New User.
-	2. -> Old user.
-
-	""";
-
-
-	""";
-	String Prompt = """
+	String menu = """
 
 	1. -> Open account.
 	2. -> Close account.
 	3. -> Deposite Money.
 	4. -> Withdraw money.
-	5. -> Check account balance .
+	5. -> account balance .
 	6. -> Transfer from one bank to another.
 	7. -> Change Pin.
 	8. -> Exit App.
 
 	""";
-	
 
 
-	System.out.println("prompt an option from the menu :" );
-	String prompt = input.nextLine();
-	
-	
-	
+System.out.print(menu);
+int inputt = input.nextInt();
 
-	
+switch(inputt){
 
-	}
+case 1: openAccount();
+break;
+case 2: closeAccount();
+break;
+case 3: depositeMoney();
+break;
+case 4: withdrawMoney();
+break;
+case 5: accountBalance();
+break;
+case 6: transferToOtherBanks();
+break;
+case 7: changePin();
+break;
+case 8: exitApp();
+break;
+default -> System.out.println("Syntex Error");
 
 
-	public void string setOpen Account ();
 
-		
+
+}
+}
+
+
+
+public static void openAccount(){
+
+System.out.print("what type of account do you want to open :");
+
+
+menu();
+}
+
+
+public static void closeAccount(){
+
+System.out.print("hello");
+
+menu();
+}
+
+
+public static void depositeMoney(){
+
+System.out.print("accountBalance - deposited amount");
+
+menu();
+}
+
+
+public static void withdrawMoney(){
+
+System.out.print("accountBalance - withdrawnamount");  
+
+menu();
+}
+
+
+public static void accountBalance(){
+
+System.out.print("accountBalance - withdrawnAmount");  
+
+menu();
 
 }
 
+public static void transferToOtherBanks(){
+
+System.out.print("accountBalance - amountToSend");  
+
+menu();
+
+}
+
+public static void changePin(){
+
+System.out.print("old Pin - new pin");  
+
+menu();
+
+}
+
+
+public static void exitApp(){
+
+System.out.print("old Pin - new pin");  
+
+menu();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
 	
+	
+
+
+	
+	
+
+
+
--- DAY 7/Java/MenstralApp.java
+++ DAY 7/Java/MenstralApp.java
@@ -17,18 +17,19 @@ public static void main(String[] arg){
 	int randomm = rando.nextInt(); 
 
 
-	int randommNumbers7 = rando.nextInt(7);
-	int randommNumbers3 = rando.nextInt(3);
+	int randommNumbers7 = rando.nextInt(5);
+	int randommNumbers3 = rando.nextInt(2);
 
 
 
 
-	System.out.print("Enter the first date of your period (Day/Month/Year) : ");
+	System.out.print("Enter the first date of your Circle (Day/Month/Year) : ");
 	String yourLastPeriod = input.nextLine();
+	System.out.println(" ");
 
 
 
-	System.out.printf("how long did was your last period last: ") ;
+	System.out.printf("how long was your last period: ") ;
 	int forHowLong = input.nextInt();
 
 	
@@ -37,7 +38,7 @@ public static void main(String[] arg){
 
 
 	DateTimeFormatter myDateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-	String myFirstTimeAndDateFormat = todaysDate.format(myDateFormat);
+	//String myFirstTimeAndDateFormat = todaysDate.format(myDateFormat);
 
 
 	
@@ -51,11 +52,12 @@ public static void main(String[] arg){
 	LocalDate nextMenstralDate = yourLastPeriodFormated.plusDays(28);
 
 
+	LocalDate ovulationDate = nextMenstralDate.minusDays(14);
 
 
-	LocalDate nextIregularMenstralDatePlus = yourLastPeriodFormated.plusDays(28).plusDays(randommNumbers7);
-	LocalDate nextIregularMenstralDateMinus = yourLastPeriodFormated.plusDays(28).minusDays(randommNumbers7);
 
+	LocalDate nextIregularMenstralDatePlus = nextMenstralDate.plusDays(randommNumbers7);
+	LocalDate nextIregularMenstralDateMinus = nextMenstralDate.minusDays(randommNumbers7);
 
 	
 
@@ -65,14 +67,20 @@ public static void main(String[] arg){
 
 
 	
-	System.out.println(myFirstTimeAndDateFormat);
-	System.out.println(yourLastPeriodFormated);
+	//System.out.println(myFirstTimeAndDateFormat);
+	//System.out.println(yourLastPeriodFormated);
 
 
-	System.out.println("Average Date for Next regular menstral flow should end " + nextMenstralDate.format(myDateFormat) );
+	System.out.println("Average Date For Next Regular Menstral Flow Should Start " + nextMenstralDate.format(myDateFormat) );
+	System.out.println(" ");
+	System.out.println("Your Next Ovulation Date " + ovulationDate.format(myDateFormat) );
+	System.out.println(" ");
 	System.out.println("And If irregular,flow Should fall between " + nextIregularMenstralDateMinus.format(myDateFormat) + " to " + nextIregularMenstralDatePlus.format(myDateFormat) );
+	System.out.println(" ");
 	System.out.println("Your fertility Period should be between " +  nextMenstralDatePlus10Days.format(myDateFormat) + " to " + nextMenstralDatePlus15Days.format(myDateFormat) );
+	System.out.println(" ");
 	System.out.println("Your Safest period is between  " +  yourLastPeriodFormated.format(myDateFormat) + " to " + safePeriod.format(myDateFormat) );
+	System.out.println(" ");
 
 
 
